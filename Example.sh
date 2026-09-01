3bab9432c56af4b6969455556a85a5cb3557602d
Zixun Zhao <204428228+Kardashevw@users.noreply.github.com>
2026-09-01 17:08:58 +0800

    build: link reproducible RNG implementation

--- Example.sh
+++ Example.sh
@@ -1,1 +1,1 @@
-g++ Example.cpp ./Micro_field_adaptive.cpp ./GetPsi_micro_field.cpp ./CosMoCal.cpp ./SampleMethod/RejectAndAcceptSample.cpp -lm -std=c++17 -O3 -g -pthread -o Example
+g++ Example.cpp ./Micro_field_adaptive.cpp ./GetPsi_micro_field.cpp ./CosMoCal.cpp ./SampleMethod/RejectAndAcceptSample.cpp ./ReproducibleRandom.cpp -lm -std=c++17 -O3 -g -pthread -o Example
