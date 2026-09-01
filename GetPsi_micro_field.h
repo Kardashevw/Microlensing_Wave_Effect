f8ac49d43b2bc2ca6ea2be73864261d558896213
Zixun Zhao <204428228+Kardashevw@users.noreply.github.com>
2026-09-01 17:07:49 +0800

    feat: route lens positions through reproducible RNG

--- GetPsi_micro_field.h
+++ GetPsi_micro_field.h
@@ -12,6 +12,7 @@
 #include <string>
 #include <random>
 #include "./spline.h"
+#include "./ReproducibleRandom.h"
 
 using std::sin;
 using std::cos;
@@ -25,11 +26,14 @@ using std::acos;
 
 using namespace std;
 
-
 #define PI acos(-1)
 
 double* Preparation4CreatPhiKappaStar(double kappa, double gamma, double kappaStar, double coeffi, int PrecisionFactor);
-double* CreatMicroLens(double SkyLimitX, double SkyLimitY, int NStar);
+
+// Keep the legacy implementation in GetPsi_micro_field.cpp untouched,
+// but route solver call sites through the deterministic implementation.
+#define CreatMicroLens(...) CreatMicroLensSeeded(__VA_ARGS__)
+
 double MicroCreatPsi(double SkyLimitX, double SkyLimitY, double kappa, double gamma, double X1, double X2, double *MicroLensCoorXY, int NStar, long*** ResNearFieldMicroIndexSum, double ResolutionL2, double*** ResFarFieldAlphaAndCenterPotential, double* X1SetL2, double* X2SetL2, double* MassSample, double AveMassTotal);
 long* NearFieldMicroIndex(double SkyLimitX, double SkyLimitY, int NStar, vector<vector<vector<long>>>& X1X2SetL1IncludeMicroIndex, double ResolutionL1, long X1L1Length, long X2L1Length, double X1L2Tmp, double X2L2Tmp);
 double* FarFieldAlphaAndCenterPotential(double* MicroLensCoorXY, int NStar, long* ResNearFieldMicroIndex, double ResolutionL2, double X1L2Tmp, double X2L2Tmp, double* MassSample, double AveMassTotal);
